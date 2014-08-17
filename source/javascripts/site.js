514e8aa3b524a53d0f0d4feeb1a09a34b3ce9b3b
Robert Lee-Cann <robert.lee-cann@stylefoundry.co.uk>
2014-08-17 12:47:13 +0100

    Sticky header working.

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,16 @@
 //= require_tree "./libs"
+//= require "./plugins"
+
+$(document).ready(function() {
+
+  var options = {
+    offset: '.main',
+    throttle: 250,
+    onInit: function() {
+      $('.headhesive img').attr('src', '/images/logo-white.png');
+    },
+  };
+
+  var stickyHeader = new Headhesive('.header-wrapper', options);
+
+});
